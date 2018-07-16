5cdfb4bd6cb5efdc3a1167e90ac4beb2d46048a4
Anton <anton@adc.sh>
2018-07-17 00:42:23 +0100

    bring structure up-to-date

--- test/spec/index.js
+++ test/spec/index.js
@@ -1,23 +1,10 @@
-import { equal, assert } from 'zoroaster/assert'
-import context, { Context } from '../context' // eslint-disable-line no-unused-vars
+import { ok } from 'zoroaster/assert'
 import eslintConfigArtdeco from '../../src'
 
-const eslintConfigArtdecoTestSuite = {
-  context,
-  'should be a function'() {
-    equal(typeof eslintConfigArtdeco, 'function')
-  },
-  'should call package without error'() {
-    assert.doesNotThrow(() => {
-      eslintConfigArtdeco()
-    })
-  },
-  /**
-   * @param {Context} api
-   */
-  async 'calls test context method'(api) {
-    await api.example()
+const T = {
+  'is an object'() {
+    ok(eslintConfigArtdeco)
   },
 }
 
-export default eslintConfigArtdecoTestSuite
+export default T
